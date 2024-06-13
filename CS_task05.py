18fe7fe924a466e5a7411e0ccc4324560ce2bff0
IMxGIRISH <119674392+Girish312@users.noreply.github.com>
2024-06-13 13:27:57 +0530

    Update CS_task05.py

--- CS_task05.py
+++ CS_task05.py
@@ -1,17 +1,17 @@
-from scapy.all import sniff, IP, TCP, UDP
+from scapy.all import sniff, IP,TCP,UDP
 def packet_callback(packet):
     if IP in packet:
-        ip_src = packet[IP].src
-        ip_dst = packet[IP].dst
-        proto = packet[IP].proto
-        payload = bytes(packet[IP].payload)
-        protocol = ''
-        if proto == 6:
-            protocol = 'TCP'
-        elif proto == 17:
-            protocol = 'UDP'
+        ip_src= packet[IP].src
+        ip_dst= packet[IP].dst
+        proto= packet[IP].proto
+        payload= bytes(packet[IP].payload)
+        protocol= ''
+        if proto== 6:
+            protocol= 'TCP'
+        elif proto== 17:
+            protocol= 'UDP'
         else:
-            protocol = 'Other'
+            protocol= 'Other'
 
         print(f"Source: {ip_src}, Destination: {ip_dst}, Protocol: {protocol}")
         print(f"Payload: {payload[:30]}...")
